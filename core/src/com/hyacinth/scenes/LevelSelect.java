1006bc86245c1973b53d14ff3983e6f0125a4b50
Warren Funk <bluetangkid@gmail.com>
2020-11-14 19:53:44 -0500

    LevelSelect actually works kinda

--- core/src/com/hyacinth/scenes/LevelSelect.java
+++ core/src/com/hyacinth/scenes/LevelSelect.java
@@ -35,8 +35,8 @@ public class LevelSelect {
             for(int j = 0; j < 6; j++){
                 subTitleFont.draw(spriteCranberry, Integer.toString(i*6 + j + 1), j*150 + 1920/2 - 5*75, (4-i)*250 + 1080/2 - 6*125);
                 if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)){
-                    if(Math.sqrt(Math.pow(Gdx.input.getY() - j*150 + 1920/2  - 5*75, 2) - Math.pow(Gdx.input.getX() - (4-i)*250 + 1080/2 - 6*125, 2)) < 80){
-                        return i*6 + j;
+                    if(Math.sqrt(Math.pow(Gdx.input.getX() - (j*150 + 1920/2  - 5*75), 2) + Math.pow(Gdx.input.getY() - ((4-i)*250 + 1080/2 - 6*125), 2)) < 80){
+                        return (2-i)*6 + j;
                     }
                 }
             }
